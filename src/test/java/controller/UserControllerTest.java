324b52c94a6bb84ff22148dd2734bd400629eeec
Francisco Savala <francisco.savala@mercadolibre.com>
2024-02-27 17:16:52 -0300

    controller T-0007

--- src/test/java/controller/UserControllerTest.java
+++ src/test/java/controller/UserControllerTest.java
@@ -1,6 +1,8 @@
 package controller;
 
 import com.example.be_java_hisp_w25_g01.controller.UserController;
+import com.example.be_java_hisp_w25_g01.dto.response.FollowersCountDTO;
+import com.example.be_java_hisp_w25_g01.dto.response.FollowersDTO;
 import com.example.be_java_hisp_w25_g01.dto.response.MessagesDTO;
 import com.example.be_java_hisp_w25_g01.service.IUserService;
 import org.junit.jupiter.api.Assertions;
@@ -11,7 +13,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.ResponseEntity;
 
-import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -53,4 +55,16 @@ public class UserControllerTest {
         Assertions.assertEquals(messegaExpected, result.getBody());
     }
 
+    //T-0007
+    @Test
+    void getFollowersCountOK(){
+        FollowersCountDTO followersCountDTO = new FollowersCountDTO(1, "user_1", 10L);
+
+        when(userService.getFollowersCount(anyInt())).thenReturn(followersCountDTO);
+
+        ResponseEntity<FollowersCountDTO> result = (ResponseEntity<FollowersCountDTO>) userController.getFollowersCount(anyInt());
+
+        Assertions.assertEquals(followersCountDTO.getFollowers_count(), result.getBody().getFollowers_count());
+
+    }
 }
