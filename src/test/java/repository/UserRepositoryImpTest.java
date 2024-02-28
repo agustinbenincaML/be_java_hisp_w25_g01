1df343150c6806faae470c1f122be266b6f6f8bd
ignapedrosa <IGNAPEDROSA2@GMAIL.COM>
2024-02-28 12:56:50 -0300

    Revert "Revert "Revert "Merge branch 'main' into Nacho"""
    This reverts commit 81a69d5c6fc20219e2a041f979b03a860f66462a.

--- src/test/java/repository/UserRepositoryImpTest.java
+++ src/test/java/repository/UserRepositoryImpTest.java
@@ -6,19 +6,11 @@ import com.example.be_java_hisp_w25_g01.repository.IUserRepository;
 import com.example.be_java_hisp_w25_g01.repository.impl.UserRepositoryImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import util.TestUtilGenerator;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import static org.mockito.Mockito.verify;
 
 
 public class UserRepositoryImpTest {
 
-
-    IUserRepository userRepository;
+    IUserRepository userRepository = new UserRepositoryImpl();
 
     @Test
     void findByIdOK(){
@@ -28,28 +20,4 @@ public class UserRepositoryImpTest {
 
         Assertions.assertEquals("martinMarquez", user.getUserName());
     }
-
-    @Test
-    void unfollowUserOk(){
-        TestUtilGenerator util = new TestUtilGenerator();
-        //Arrange
-        User user1 = util.getUser();
-        User user2 = new User(5,"leanSaracco", new ArrayList<>(List.of()), new ArrayList<>(List.of(1,2,3,4)),new ArrayList<>(List.of(4,5)));
-
-
-        //act
-
-        //assert
-    }
-    void followUserOkTest(){
-        Integer userId = 1;
-        Integer userIdToFollow = 4;
-
-        userRepository.followUser(userId, userIdToFollow);
-
-        User user = userRepository.findById(userId).get(); //preguntar si esta ok llamar al metodo findById
-
-        Assertions.assertTrue(user.getFollowed().contains(userIdToFollow));
-    }
-
 }
