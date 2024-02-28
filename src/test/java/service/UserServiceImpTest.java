948997127d0a221bded1bc7c016e369ab47a7ae9
Agustin Espeche <agustin.espeche@mercadolibre.com>
2024-02-28 09:25:21 -0300

    haciendo test del service unFollowUser

--- src/test/java/service/UserServiceImpTest.java
+++ src/test/java/service/UserServiceImpTest.java
@@ -1,6 +1,7 @@
 package service;
 
 import com.example.be_java_hisp_w25_g01.dto.response.MessagesDTO;
+import com.example.be_java_hisp_w25_g01.dto.response.UserDTO;
 import com.example.be_java_hisp_w25_g01.repository.IPostRepository;
 import com.example.be_java_hisp_w25_g01.repository.IUserRepository;
 import com.example.be_java_hisp_w25_g01.service.impl.UserServiceImpl;
@@ -10,6 +11,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import util.TestUtilGenerator;
 
 import static org.mockito.Mockito.when;
 
@@ -28,10 +30,11 @@ public class UserServiceImpTest {
     @Test
     void unfollowUserOk(){
         //arrange
-        Integer userId = 1;
-        Integer userIdToUnfollow = 4;
-        MessagesDTO messageSpected = new MessagesDTO("User with id: " + userId + " is now unfollowing user with id: " + userIdToUnfollow);
-        when(userRepository.findById(userId)).thenReturn(userId);
+        TestUtilGenerator utilGenerator = new TestUtilGenerator();
+        UserDTO user = utilGenerator.getUserDTO();
+        UserDTO userIdToUnfollow = utilGenerator.getUserDTO();
+        MessagesDTO messageSpected = new MessagesDTO("User with id: " + user.getUser_id() + " is now unfollowing user with id: " + userIdToUnfollow.getUser_id());
+        when(userRepository.findById(user.getUser_id())).thenReturn(user);
 
         //act
 
