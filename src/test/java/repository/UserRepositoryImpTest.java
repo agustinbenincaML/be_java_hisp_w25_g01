1935ad5249d36f85d24fb1400885ff30dde05ebd
Agustin Espeche <agustin.espeche@mercadolibre.com>
2024-02-28 12:26:44 -0300

    repository test unfollowUser, in progress

--- src/test/java/repository/UserRepositoryImpTest.java
+++ src/test/java/repository/UserRepositoryImpTest.java
@@ -6,6 +6,11 @@ import com.example.be_java_hisp_w25_g01.repository.IUserRepository;
 import com.example.be_java_hisp_w25_g01.repository.impl.UserRepositoryImpl;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import util.TestUtilGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 
 public class UserRepositoryImpTest {
@@ -20,5 +25,18 @@ public class UserRepositoryImpTest {
         User user = userRepository.findById(id).get();
 
         Assertions.assertEquals("martinMarquez", user.getUserName());
+    }
+
+    @Test
+    void unfollowUserOk(){
+        TestUtilGenerator util = new TestUtilGenerator();
+        //Arrange
+        User user1 = util.getUser();
+        User user2 = new User(5,"leanSaracco", new ArrayList<>(List.of()), new ArrayList<>(List.of(1,2,3,4)),new ArrayList<>(List.of(4,5)));
+
+
+        //act
+
+        //assert
     }
 }
